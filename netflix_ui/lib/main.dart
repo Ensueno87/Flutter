6194333f3097cdeba01b2f9bd0096e622e3fd8a9
Ensueno <seongkwon87@naver.com>
2020-07-06 14:15:55 +0900

    TopBar 작성

--- netflix_ui/lib/main.dart
+++ netflix_ui/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_ui/screen/home_screen_dart.dart';
 import 'package:netflix_ui/widget/bottom_bar.dart';
 
 void main() {
@@ -25,7 +26,7 @@ class _MyAppState extends State<MyApp> {
           body: TabBarView(
           physics: NeverScrollableScrollPhysics(),
           children: <Widget>[
-            Container(child: Center(child: Text('home'),),),
+            HomeScreen(),
             Container(child: Center(child: Text('search'),),),
             Container(child: Center(child: Text('content'),),),
             Container(child: Center(child: Text('list'),),),
